eb05bbb62eaec1d2cf7b5be83c41f1d9ba6b8d30
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-22 16:28:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,9 @@ class Artist
   attr_reader :songs
 
  extend Memorable::ClassMethods
- include Memorable::InstanceMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
   @@artists = []
 
